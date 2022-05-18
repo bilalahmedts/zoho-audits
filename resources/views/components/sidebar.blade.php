f86a577a4185aff56de583671b20f00dceecc548
Bilal Ahmed <93002947+bilalahmedts@users.noreply.github.com>
2022-05-18 14:46:56 -0500

    Update sidebar.blade.php

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -9,9 +9,9 @@
             </a>
         </li>
         <li class="nav-header">ZOHO AUDITS</li>
-        
         <li class="nav-item">
-            <a href="{{ route('audits.create') }}" class="nav-link {{ request()->is('audits/create') ? 'active' : '' }}">
+            <a href="{{ route('audits.create') }}"
+                class="nav-link {{ request()->is('audits/create') ? 'active' : '' }}">
                 <i class="nav-icon fas fa-tasks"></i>
                 <p>
                     Create Zoho Audit
@@ -26,36 +26,37 @@
                 </p>
             </a>
         </li>
-        @if (in_array(Auth::user()->roles[0]->name, ['Super Admin','Director','Manager', 'Team Lead']))
         <li class="nav-item">
-            <a href="{{ route('audits.audit-report') }}" class="nav-link {{ request()->is('audits/audit-report') ? 'active' : '' }}">
+            <a href="{{ route('audits.audit-report') }}"
+                class="nav-link {{ request()->is('audits/audit-report') ? 'active' : '' }}">
                 <i class="far fa-file nav-icon"></i>
                 <p>View Report</p>
             </a>
         </li>
-        @endif
         @if (in_array(Auth::user()->roles[0]->name, ['Super Admin']))
-        <li class="nav-item {{ request()->is('users','roles') ? 'menu-open' : '' }}">
-            <a href="#" class="nav-link {{ request()->is('users','roles') ? 'active' : '' }}">
-                <i class="nav-icon fas fa-users"></i>
-                <p>
-                    Users Management
-                    <i class="fas fa-angle-left right"></i>
-                </p>
-            </a>
-            <ul class="nav nav-treeview">
-                <li class="nav-item">
-                    <a href="{{ route('users.index') }}" class="nav-link {{ request()->is('users') ? 'active' : '' }}">
-                        <i class="far fa-circle nav-icon"></i>
-                        <p>Users</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{ route('roles.index') }}" class="nav-link {{ request()->is('roles') ? 'active' : '' }}">
-                        <i class="far fa-circle nav-icon"></i>
-                        <p>Roles</p>
-                    </a>
-                </li>
-                @endif
-            </ul>
+            <li class="nav-item {{ request()->is('users', 'roles') ? 'menu-open' : '' }}">
+                <a href="#" class="nav-link {{ request()->is('users', 'roles') ? 'active' : '' }}">
+                    <i class="nav-icon fas fa-users"></i>
+                    <p>
+                        Users Management
+                        <i class="fas fa-angle-left right"></i>
+                    </p>
+                </a>
+                <ul class="nav nav-treeview">
+                    <li class="nav-item">
+                        <a href="{{ route('users.index') }}"
+                            class="nav-link {{ request()->is('users') ? 'active' : '' }}">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Users</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{ route('roles.index') }}"
+                            class="nav-link {{ request()->is('roles') ? 'active' : '' }}">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Roles</p>
+                        </a>
+                    </li>
+        @endif
+    </ul>
 </nav>
