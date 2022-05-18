6f5a8b44b16105880c1f71ab4690988f6b0ebce5
Bilal Ahmed <93002947+bilalahmedts@users.noreply.github.com>
2022-05-18 17:06:26 -0500

    updates....

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -1,12 +1,19 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Http\Controllers\Controller;
+
+use App\Models\User;
+use App\Models\Audit;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('dashboard');
+        $passedEvaluations = Audit::where('evaluationStatus', 'Passed')->count('evaluationStatus');
+        $failedEvaluations = Audit::where('evaluationStatus', 'Failed')->count('evaluationStatus');
+        $totalEvaluations = Audit::count('evaluationStatus');
+        return view('dashboard')->with(compact('passedEvaluations','failedEvaluations','totalEvaluations'));
     }
 
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,44 @@
 @extends('layouts.app')
 @section('title', 'Dashboard')
 @section('content')
+<div class="row">
+
+    <div class="col">
+        <div class="row">
+            <div class="col-md-4">
+                <!-- small box -->
+                <div class="small-box bg-success">
+                    <div class="inner">
+                        <h3>{{ $passedEvaluations }}</h3>
+                        <p>Number of Passed Evaluations</p>
+                    </div>
+                </div>
+            </div>
+            <!-- ./col -->
+            <div class="col-md-4">
+                <!-- small box -->
+                <div class="small-box bg-danger">
+                    <div class="inner">
+                        <h3>{{ $failedEvaluations }}</h3>
+                        <p>Number of Failed Evaluations</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <!-- small box -->
+                <div class="small-box bg-info">
+                    <div class="inner">
+                        <h3>{{ $totalEvaluations }}</h3>
+                        <p>Number of Total Evaluations</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+  </div>
+
 
 @endsection
 
